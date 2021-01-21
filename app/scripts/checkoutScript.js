d84b520ca182280b2b4accf2976dbc0e2b3571d7
Heisenburn <marcinrudnik1996@gmail.com>
2021-01-21 12:01:37 +0100

    added success.html fetch + minor styling

--- app/scripts/checkoutScript.js
+++ app/scripts/checkoutScript.js
@@ -8,11 +8,14 @@ const setInvalidInput = (elementInDOM, errorCommunication) => { // DRY
 };
 
 
+ 
+
 document.querySelector('form').addEventListener('submit', (e)=>{
   e.preventDefault();
 
 
   document.querySelectorAll('input').forEach((item)=>{
+      
     if (item.value === '') {
       setInvalidInput(item, 'Cannot be empty!');
     }
@@ -20,47 +23,51 @@ document.querySelector('form').addEventListener('submit', (e)=>{
 
 
   const emailInput = document.querySelector('#email');
-
   if (emailInput.value.includes('@') === false ) {
     setInvalidInput(emailInput, 'Not a valid email address!');
   }
 
 
   const postCodeInput = document.querySelector('#postalCode');
-
   if (postCodeInput.value.length !== 5) {
     setInvalidInput(postCodeInput, 'Not a valid postal code');
   }
 
   const phoneNumberInput = document.querySelector('#phoneNumber');
-
   if (phoneNumberInput.value.length !== 10) {
     setInvalidInput(phoneNumberInput, 'Not a valid phone number');
   }
 
   const creditCardInput = document.querySelector('#creditCardNumber');
-
   if (creditCardInput.value.length !== 19 || [...creditCardInput.value].reduce((n, x) => n + (x === '-'), 0) !== 3 ) { // counting occurences of "-" in credit card number
     setInvalidInput(creditCardInput, 'Not a valid credit card number');
   }
 
 
   const securityCodeInput = document.querySelector('#securityCode');
-
   if (securityCodeInput.value.length !== 3) {
     setInvalidInput(securityCodeInput, 'Not a valid security code');
   }
 
   const expirationDateInput = document.querySelector('#expirationDate');
-
   if (expirationDateInput.value.length !== 5 || expirationDateInput.value.includes('/') === false) {
     setInvalidInput(expirationDateInput, 'Not a valid expiration date');
   }
 
 
   if (validateErrors === 0) {
-    alert('gitara siema');
+   
+    fetch('/purchase-success.html')
+  .then(function(response) {
+    return response.text();
+  })
+  .then(function(contentsOfPurchaseSuccess) {
+    document.querySelector('body').innerHTML = contentsOfPurchaseSuccess;
+  });
+    
   }
 });
 
+
+ 
 
